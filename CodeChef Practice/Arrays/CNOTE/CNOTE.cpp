57d1728dddf73dd3ccb1e03eef3e4f0bd14ae72b
AnandDev006 <anand.dev.006@gmail.com>
2019-04-27 08:57:18 +0530

    retry

--- CodeChef Practice/Arrays/CNOTE/CNOTE.cpp
+++ CodeChef Practice/Arrays/CNOTE/CNOTE.cpp
@@ -1,27 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define tr(c, i) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c, x) ((c).find(x) != (c).end())
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define X first
+#define Y second
 
 using namespace std;
 
-int main(){
-    int T;
+inline void debug()
+{
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll T;
     cin >> T;
-    int X, Y, K, N;
-    int P, C;
-    bool flag;
-    while(T--){
+    while (T--)
+    {
+        ll X, Y, K, N;
         cin >> X >> Y >> K >> N;
-        flag = false;
-        while( N--){
+        bool flag = false;
+        while (N--)
+        {
+            ll P, C;
             cin >> P >> C;
-            if( C <= K && P >= ( X - Y ) ){
+            if (P >= (X - Y) && C <= K)
+            {
                 flag = true;
                 break;
             }
         }
-        if( flag)
+        if (flag)
+        {
             cout << "LuckyChef" << endl;
+        }
         else
+        {
             cout << "UnluckyChef" << endl;
+        }
     }
+
     return 0;
 }
--- CodeChef Practice/Arrays/SALARY/SALARY.cpp
+++ CodeChef Practice/Arrays/SALARY/SALARY.cpp
@@ -1,23 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define tr(c, i) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c, x) ((c).find(x) != (c).end())
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define X first
+#define Y second
 
 using namespace std;
 
-int main(){
-    int T;
+inline void debug()
+{
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> ii;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll T;
     cin >> T;
-    int N;
-    int minim, sum;
-    while( T--){
+    while (T--)
+    {
+        ll temp, N, tot = 0, minim = INT_MAX;
         cin >> N;
-        int W[N];
-        minim = INT32_MAX;
-        sum = 0;
-        for( int i = 0 ; i < N ; ++i){
-            cin >> W[i];
-            minim = min( minim, W[i]);
-            sum += W[i];
+        for (ll i = 0; i < N; ++i)
+        {
+            cin >> temp;
+            tot += temp;
+            minim = min(minim, temp);
         }
-        cout << sum - N*minim << endl;
+        cout << (tot - N * minim) << endl;
     }
+
     return 0;
 }
